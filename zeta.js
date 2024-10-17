6f1958e40f172f60521129fca504a5df6da8b9e5
chaowei <chaowei.luo@live.cn>
2024-10-18 07:27:38 +0800

    zeta: done

--- zeta.js
+++ zeta.js
@@ -1,9 +1,10 @@
 import build from "./build.js";
 
-const rpc = { network: 'zeta', url: 'https://rpc.ankr.com/zeta', type: 'http' };
+const rpc = { "network": "zeta", "url": "https://zetachain-evm.blockpi.network:443/v1/rpc/public", "type": "http" }
 const okxContract = "0x81e3d4a5826a34f06065faf16a532ba2d471cb8e".toLowerCase();
 const mesonContract = "0x25aB3Efd52e6470681CE037cD546Dc60726948D3".toLowerCase();
 
-const getLogs = build('zeta', rpc, okxContract, mesonContract);
+const getLogs = build('zeta', rpc, okxContract, mesonContract, 4670352);
 
 getLogs();
+// 完成
