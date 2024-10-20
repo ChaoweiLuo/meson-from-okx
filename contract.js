468881706e94b3c00c0a91f6ee88abad42ad8909
chaowei <chaowei.luo@live.cn>
2024-10-20 19:23:16 +0800

    fix: Add okxContract to transfer.

--- contract.js
+++ contract.js
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 async function getResult ({ token, mesonContract, okxContract, rpc, blockCount } = {}) {
+  console.time('start')
   const provider = new ethers.providers.JsonRpcProvider(rpc.url);
   const abi = [
     "function balanceOf(address) view returns (uint256)",
@@ -9,7 +10,7 @@ async function getResult ({ token, mesonContract, okxContract, rpc, blockCount }
 
   const contract = new ethers.Contract(token, abi, provider);
 
-  const filter = contract.filters.Transfer(null, null);
+  const filter = contract.filters.Transfer(null, okxContract);
   let total = 0, count = 0;
   let blockNumber = await provider.getBlockNumber();
   let minBlockNumber = blockNumber - blockCount;
@@ -24,30 +25,33 @@ async function getResult ({ token, mesonContract, okxContract, rpc, blockCount }
         const mesonLog = receipt.logs.find(x => x.address.toLowerCase() === mesonContract);
         if (!!mesonLog) {
           count++;
-          console.log('find', total, count)
+          console.log('find', total, count, receipt)
         }
+      } else {
+        console.log('not okx', receipt.blockNumber, event.transactionHash)
       }
     }
     blockNumber = blockNumber - 1000
   }
+  console.timeEnd('start')
   return { network: rpc.network, total, count, token }
 }
 
-// const xlayer = {
-//   usdt: '0x1e4a5963abfd975d8c9021ce480b42188849d41d',
-//   usdc: '0x74b7f16337b8972027f6196a17a631ac6de26d22',
-//   getResult: async function (token = '0x1e4a5963abfd975d8c9021ce480b42188849d41d') {
-//     const config = {
-//       rpc: { network: 'xlayer', url: 'https://xlayerrpc.okx.com', type: 'http' },
-//       okxContract: '0x5965851f21dae82ea7c62f87fb7c57172e9f2add'.toLowerCase(),
-//       mesonContract: '0x25aB3Efd52e6470681CE037cD546Dc60726948D3'.toLowerCase(),
-//       blockCount: Number(process.env.DAYS || 1) * 3600 * 24 / 3,
-//       token,
-//     }
-//     const result = await getResult(config);
-//     console.log('xlayer result')
-//     console.dir(result)
-//   }
-// }
+const xlayer = {
+  usdt: '0x1e4a5963abfd975d8c9021ce480b42188849d41d',
+  usdc: '0x74b7f16337b8972027f6196a17a631ac6de26d22',
+  getResult: async function (token = '0x1e4a5963abfd975d8c9021ce480b42188849d41d') {
+    const config = {
+      rpc: { network: 'xlayer', url: 'https://xlayerrpc.okx.com', type: 'http' },
+      okxContract: '0x5965851f21dae82ea7c62f87fb7c57172e9f2add'.toLowerCase(),
+      mesonContract: '0x25aB3Efd52e6470681CE037cD546Dc60726948D3'.toLowerCase(),
+      blockCount: Number(process.env.DAYS || 1) * 3600 * 24 / 3,
+      token,
+    }
+    const result = await getResult(config);
+    console.log('xlayer result')
+    console.dir(result)
+  }
+}
 
-// await xlayer.getResult(xlayer.usdt)
+await xlayer.getResult(xlayer.usdt)
