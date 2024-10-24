ff69ea0162a55ac53d8ccb7b2304a2aa79629735
chaowei <chaowei.luo@live.cn>
2024-10-24 16:18:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import load from './load.js';
-import check from './check.js';
 
-const args = {
-  chainName: 'xlayer',
-  startBlock: 5602448, // 5500000,
-  endBlock: 5612448,
-}
+// const args = { chainName: 'xlayer', startBlock: 5602448, endBlock: 5612448, }
+// const args = { chainName: 'xlayer', startBlock: 5500000, endBlock: 5612448, }
+// const args = { chainName: 'arbitrum', startBlock: 267070739, endBlock: 267080739, }
+const args = { chainName: 'arbitrum', startBlock: 267000739, endBlock: 267080739, }
+
 
 await load(args);
